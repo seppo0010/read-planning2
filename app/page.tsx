aa8b09e7e048abb41e4c5becadaa762bea4a1a20
Sebastian Waisbrot <seppo0010@gmail.com>
2024-07-09 14:17:57 -0300

    debug

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { useState } from "react";
+import useSWR from 'swr'
 import * as dfd from "danfojs"
+import { useEffect } from 'react';
 
 const apiKey = `AIzaSyA6LYbb-Xlq0lkeCLMb88sSWFVE9beq1Z0`
 const spreadsheetId = '1S6KMZCx4slqZm0yLF8cKqVmyFQjsO8hfZLebbz2wAsM';
@@ -11,13 +12,19 @@ const LENGTH = 'Length'
 const DONE_AT = 'Done at'
 const TODAY = new Date() // new Date(2024, 6, 15);
 
-export default function Home() {
-  useState(async () => {
-    // https://docs.google.com/spreadsheets/d/1S6KMZCx4slqZm0yLF8cKqVmyFQjsO8hfZLebbz2wAsM/edit?gid=0#gid=0
-    const req = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`)
-    const data = (await req.json()).values;
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
-    const df = new dfd.DataFrame(data.slice(1), { columns: data[0] }).asType(LENGTH, "int32");
+export default function Home() {
+  // https://docs.google.com/spreadsheets/d/1S6KMZCx4slqZm0yLF8cKqVmyFQjsO8hfZLebbz2wAsM/edit?gid=0#gid=0
+  const { data, error } = useSWR(
+    `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`,
+    fetcher
+  )
+
+  useEffect(() => {
+    if (!data) return;
+    const rows = data.values;
+    const df = new dfd.DataFrame(rows.slice(1), { columns: rows[0] }).asType(LENGTH, "int32");
     const layout = {
       title: "Progreso",
       xaxis: {
@@ -48,14 +55,16 @@ export default function Home() {
     const new_df = (dfd.concat({ dfList: [due, done], axis: 0 }).sortValues(DATE).fillNa(0) as dfd.DataFrame)
       .setIndex({ column: DATE, drop: true })
       .cumSum({ axis: 0 });
-    new_df.print();
     const isFuture = new dfd.Series(new_df.index.map((x) => new Date(x) > TODAY), { columns: ['isFuture'] });
+
     (dfd.concat({ dfList: [new_df, isFuture, new dfd.Series(new_df.index, { columns: [DATE] })], axis: 1 }) as dfd.DataFrame)
       .setIndex({ column: DATE, drop: true })
       .apply(([x1, x2, isFuture]: [number, number, boolean]) => (isFuture ? [x1, null, isFuture] : [x1, x2, isFuture]))
       .plot("plot_div").line({ config, layout });
-  })
+  }, [data, error])
 
+  if (error) return 'Failed to load'
+  if (!data) return 'Loading...'
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div id="plot_div"></div>
